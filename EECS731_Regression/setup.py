0fa85691450e059c71d2617c6c9590979eeeac09
Vivek Tallavajhala <vivtalla@ku.edu>
2018-10-17 11:12:56 -0500

    Add CookieCutter Project Structure

---
+++ EECS731_Regression/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project utilizing regression on NBA dataset for EECS 731',
+    author='Vivek Tallavajhala',
+    license='',
+)
